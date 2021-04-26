5f86ac88acf24a209ba169f3e3a64e353825534c
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-04-25 22:34:30 -0500

    Removed Bank App and references to it.

--- src/main/java/com/meritamerica/assignment6/Assignment6Application.java
+++ src/main/java/com/meritamerica/assignment6/Assignment6Application.java
@@ -3,12 +3,9 @@ package com.meritamerica.assignment6;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.meritamerica.assignment6.models.MeritAmericaBankApp;
-
 @SpringBootApplication
 public class Assignment6Application {
 	public static void main(String[] args) {
-		MeritAmericaBankApp.load();
 		SpringApplication.run(Assignment6Application.class, args);
 	}
 }
--- src/main/java/com/meritamerica/assignment6/models/MeritAmericaBankApp.java
+++
@@ -1,7 +0,0 @@
-package com.meritamerica.assignment6.models;
-
-public class MeritAmericaBankApp {
-	public static void load() {
-		//MeritBank.readFromFile("src/test/testMeritBank_good.txt");
-	}
-}
